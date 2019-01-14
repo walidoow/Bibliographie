e083afd12b4969047c1d6c57312c0c38c8e3e811
walidoow <31325808+walidoow@users.noreply.github.com>
2019-01-13 20:40:50 -0500

    Initial commit

---
+++ Ecole/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Ecole
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String author = textBox1.Text;
+            String title = textBox2.Text;
+            String link = textBox3.Text;
+            int day = monthCalendar1.SelectionStart.Day;
+            Mois month = (Mois)monthCalendar1.SelectionStart.Month;
+            int year = monthCalendar1.SelectionStart.Year;
+
+            //OUTPUT
+            richTextBox1.Text = author + ". " + title + ", [En ligne]. Adresse URL: " + link + " (Page consultée le " + day + " " + MoisManager.getStringMois(month) + " " + year + ").";
+            //AUTHOR
+            int index_author = richTextBox1.Text.IndexOf(author);
+            int length_author = author.Length;
+            richTextBox1.Select(index_author, length_author);
+            //TITLE
+            int index_title = richTextBox1.Text.IndexOf(title);
+            int length_title = title.Length;
+            richTextBox1.Select(index_title, length_title);
+            richTextBox1.SelectionFont = new Font(richTextBox1.Font, FontStyle.Italic);
+            //LINK
+            int index_link = richTextBox1.Text.IndexOf(link);
+            int length_link = link.Length;
+            richTextBox1.Select(index_link, length_link);
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            richTextBox1.Text = "";
+        }
+    }
+}
---
+++ Ecole/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ecole
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+    public enum Mois
+    {
+        JANVIER = 1,
+        FEVRIER = 2,
+        MARS = 3,
+        AVRIL = 4,
+        MAI = 5,
+        JUIN = 6,
+        JUILLET = 7,
+        AOUT = 8,
+        SEPTEMBRE = 9,
+        OCTOBRE = 10,
+        NOVEMBRE = 11,
+        DECEMBRE = 12
+    }
+    public class MoisManager
+    {
+        public static String getStringMois(Mois mois)
+        {
+            switch (mois)
+            {
+                case Mois.JANVIER:
+                    return "janvier";
+                case Mois.FEVRIER:
+                    return "février";
+                case Mois.MARS:
+                    return "mars";
+                case Mois.AVRIL:
+                    return "avril";
+                case Mois.MAI:
+                    return "mai";
+                case Mois.JUIN:
+                    return "juin";
+                case Mois.JUILLET:
+                    return "juillet";
+                case Mois.AOUT:
+                    return "août";
+                case Mois.SEPTEMBRE:
+                    return "septembre";
+                case Mois.OCTOBRE:
+                    return "octobre";
+                case Mois.NOVEMBRE:
+                    return "novembre";
+                case Mois.DECEMBRE:
+                    return "décembre";
+            }
+            return "";
+        }
+    }
+}
